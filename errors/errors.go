61560192240d0fc59578f7f85229a081e00e4b11
atsumarukun <atys.smz@gmail.com>
2026-03-29 17:44:44 +0900

    create: err型を定義.

---
+++ errors/errors.go
@@ -0,0 +1,41 @@
+package errors
+
+import (
+	"fmt"
+)
+
+type err struct {
+	code    ErrorCode
+	message string
+	pcs     programCounters
+}
+
+func New(code ErrorCode, message string) error {
+	return &err{code, message, callers()}
+}
+
+func (e *err) Error() string {
+	return fmt.Sprintf("%s: %s", e.code, e.message)
+}
+
+func (e *err) Format(f fmt.State, verb rune) {
+	switch verb {
+	case 'v', 's':
+		switch {
+		case f.Flag('+'):
+			fmt.Fprintf(f, "%s\n\n%v", e.Error(), e.pcs)
+		default:
+			fmt.Fprintln(f, e.Error())
+		}
+	default:
+		fmt.Fprintf(f, "%%!%c(%T=%v)\n", verb, e, e.Error())
+	}
+}
+
+func (e *err) Code() ErrorCode {
+	return e.code
+}
+
+func (e *err) Message() string {
+	return e.message
+}
